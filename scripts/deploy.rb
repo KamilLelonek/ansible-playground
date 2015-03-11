d80803dfa51e521a0649a79b97cb767963702267
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-11 18:31:52 +0100

    Allow params to be passed while deploying

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 playbook = ARGV[0] || 'main'
-system "ansible-playbook -i hosts playbooks/#{playbook}.yml -u root"
+system "ansible-playbook -i hosts playbooks/#{playbook}.yml -u root $@"
