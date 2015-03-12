0aaadf10051ca44cb3bc07c0bb9ab21f26cfbd15
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-12 16:42:10 +0100

    Improved deployment script

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
 ARGV[0] ||= 'main'
-system "ansible-playbook -i hosts playbooks/#{ARGV[0]}.yml -u root #{ARGV[1]}"
+system "ansible-playbook -i hosts playbooks/#{ARGV[0]}.yml -u root #{ARGV.drop(1).join(' ')}"
